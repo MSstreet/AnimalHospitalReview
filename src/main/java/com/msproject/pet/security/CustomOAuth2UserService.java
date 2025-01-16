91331079e9469b4a077489269358509df257dc17
MSstreet <cortm5pp@gmail.com>
2025-01-16 22:38:27 +0900

    카카오 소셜 로그인

---
+++ src/main/java/com/msproject/pet/security/CustomOAuth2UserService.java
@@ -0,0 +1,22 @@
+package com.msproject.pet.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        log.info("userRequest: " + userRequest);
+        return super.loadUser(userRequest);
+    }
+}
