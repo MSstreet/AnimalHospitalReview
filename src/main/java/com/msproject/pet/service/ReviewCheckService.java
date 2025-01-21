44e04faf78f280cd7c4b7e78e74f3e32afd6c88b
MSstreet <cortm5pp@gmail.com>
2025-01-21 23:25:32 +0900

    OCR

---
+++ src/main/java/com/msproject/pet/service/ReviewCheckService.java
@@ -0,0 +1,24 @@
+package com.msproject.pet.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewCheckService {
+
+//    private final Tesseract tesseract;
+//
+//    public ReviewCheckService() {
+//        tesseract = new Tesseract();
+//        tesseract.setDatapath("C:/Program Files/Tesseract-OCR/tessdata"); // Tesseract 데이터 경로
+//        tesseract.setLanguage("eng"); // 사용할 언어 설정
+//    }
+//
+//    public String extractText(File imageFile) {
+//        try {
+//            return tesseract.doOCR(imageFile);
+//        } catch (TesseractException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+}
