f26b1444f2c524ff0931d27222ba271a38c9f755
MSstreet <cortm5pp@gmail.com>
2025-02-10 22:55:38 +0900

    .

--- src/main/java/com/msproject/pet/service/ReportService.java
+++ src/main/java/com/msproject/pet/service/ReportService.java
@@ -1,11 +1,20 @@
 package com.msproject.pet.service;
-
 import com.msproject.pet.entity.ReportEntity;
+import com.msproject.pet.model.Header;
+import com.msproject.pet.model.Pagination;
+import com.msproject.pet.model.SearchCondition;
 import com.msproject.pet.repository.ReportRepository;
+
 import com.msproject.pet.web.dtos.ReportDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -17,5 +26,34 @@ public class ReportService {
     public ReportDto insertReport(ReportDto reportDto) {
         ReportEntity reportEntity = reportRepository.save(ReportEntity.of(reportDto));
         return ReportDto.fromEntity(reportEntity);
+    }
+
+    public Header<List<ReportDto>> getReportList(Pageable pageable, SearchCondition searchCondition) {
+        List<ReportDto> dtos = new ArrayList<>();
+        Page<ReportEntity> reportEntities = reportRepository.findAll(pageable);
+
+        for(ReportEntity reportEntity : reportEntities) {
+            ReportDto dto = ReportDto.fromEntity(reportEntity);
+            dtos.add(dto);
+        }
+
+        Pagination pagination = new Pagination(
+                (int)reportEntities.getTotalElements()
+                , pageable.getPageNumber() + 1
+                , pageable.getPageSize()
+                , 10
+        );
+
+        return Header.OK(dtos, pagination);
+    }
+
+    public ReportDto getReport(Long id) {
+        ReportEntity entity = reportRepository.findById(id).orElseThrow(() -> new RuntimeException("신고글을 찾을 수 없습니다."));
+        return ReportDto.fromEntity(entity);
+    }
+
+    public void delete(Long id) {
+        ReportEntity entity = reportRepository.findById(id).orElseThrow(() -> new RuntimeException("신고글을 찾을 수 없습니다."));
+        reportRepository.delete(entity);
     }
 }
--- src/main/java/com/msproject/pet/web/ReportController.java
+++ src/main/java/com/msproject/pet/web/ReportController.java
@@ -1,10 +1,16 @@
 package com.msproject.pet.web;
 
+import com.msproject.pet.model.Header;
+import com.msproject.pet.model.SearchCondition;
 import com.msproject.pet.service.ReportService;
 import com.msproject.pet.web.dtos.ReportDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @Slf4j
@@ -18,4 +24,11 @@ public class ReportController {
     public void insertReport(@RequestBody  ReportDto reportDto){
         reportService.insertReport(reportDto);
     }
+
+    @GetMapping("/")
+    public Header<List<ReportDto>> getReportList(@PageableDefault(sort = {"idx"}) Pageable pageable,
+                                                 SearchCondition searchCondition) {
+        return reportService.getReportList(pageable, searchCondition);
+    }
+
 }
