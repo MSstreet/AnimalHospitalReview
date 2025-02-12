dbc913d3eb5ccbcac735237e05492174cb38e6ba
MSstreet <cortm5pp@gmail.com>
2025-02-12 23:23:28 +0900

    .

---
+++ src/main/java/com/msproject/pet/web/HelpfulController.java
@@ -0,0 +1,21 @@
+package com.msproject.pet.web;
+
+import com.msproject.pet.entity.WishEntity;
+import com.msproject.pet.service.WishService;
+import com.msproject.pet.web.dtos.WishDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class HelpfulController {
+
+
+    private final HelpfulService helpfulService;
+
+    @PostMapping("/")
+    public HelpfulEntity createHelpfulInfo(@RequestBody HelpfulDto helpfulDto){
+
+
+    }
+}
