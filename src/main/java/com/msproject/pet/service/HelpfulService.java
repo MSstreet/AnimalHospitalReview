9ed2998d91e70efc75bf6f3d0a98fe7cdcd9bec2
MSstreet <cortm5pp@gmail.com>
2025-03-06 23:07:13 +0900

    .

--- src/main/java/com/msproject/pet/service/HelpfulService.java
+++ src/main/java/com/msproject/pet/service/HelpfulService.java
@@ -17,12 +17,10 @@ import java.util.Optional;
 public class HelpfulService {
 
     private final HelpfulRepository helpfulRepository;
-    private final HelpfulRepositoryCustom helpfulRepositoryCustom;
     private final ReviewRepository reviewRepository;
     private final UserRepository userRepository;
 
     public HelpfulDto createHelpful(HelpfulDto helpfulDto) {
-        boolean chk = checkWish(helpfulDto.getReviewNum(),helpfulDto.getUserNum());
 
         Optional<ReviewEntity> reviewEntity = reviewRepository.findById(helpfulDto.getReviewNum());
         ReviewEntity review = reviewEntity.orElseThrow();
@@ -30,37 +28,9 @@ public class HelpfulService {
         Optional<UserEntity> userEntity = userRepository.findById(helpfulDto.getUserNum());
         UserEntity user = userEntity.orElseThrow();
 
-        if(chk) {
-            helpfulRepository.save(HelpfulEntity.of(helpfulDto, review, user));
-        }else{
-            updateHelpful(helpfulDto);
-        }
+        helpfulRepository.save(HelpfulEntity.of(helpfulDto, review, user));
 
         return helpfulDto;
     }
 
-    public Boolean checkWish(Long uid, Long rid) {
-
-        if(helpfulRepositoryCustom.findOneHelpful(uid, rid) == null){
-            return true;
-        }
-        return false;
-    }
-
-    public HelpfulDto updateHelpful(HelpfulDto helpfulDto) {
-
-        HelpfulEntity entity = helpfulRepositoryCustom.findOneHelpful(helpfulDto.getUserNum(),helpfulDto.getReviewNum());
-        int postHelpfulState = helpfulDto.getHelpFul();
-        int preHelpfulState = entity.getHelpFul();
-
-        if(postHelpfulState == 1){
-            // 추가
-        }else if (postHelpfulState == 2){
-            // 추가
-        }
-
-        entity.changeHelpfulState(helpfulDto.getHelpFul());
-        entity.setUpdatedAt(LocalDateTime.now());
-        return HelpfulDto.fromEntity(helpfulRepository.save(entity));
-    }
 }
